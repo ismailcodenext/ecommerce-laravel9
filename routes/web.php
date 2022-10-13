0b15e824ed3f9282f19f5ba23f3924ea02ad1b06
codenextitD <engr.ismail348@gmail.com>
2022-10-13 21:08:14 +0600

    login ans signup auth system setum

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +14,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 Route::get('/', \App\Http\Livewire\HomeComponent::Class)->name('home.index');
 Route::get('/shop', \App\Http\Livewire\ShopComponent::class)->name('shop');
 Route::get('/cart', \App\Http\Livewire\CartComponent::class)->name('shop.cart');
 Route::get('/checkout',\App\Http\Livewire\CheckoutComponent::class)->name('shop.checkout');
+
+//Route::get('/dashboard', function () {
+//    return view('dashboard');
+//})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/user/dashboard',\App\Http\Livewire\User\UserDashboardComponent::class)->name('user.dashboard');
+});
+Route::middleware(['auth', 'authadmin'])->group(function(){
+    Route::get('/admin/dashboard',\App\Http\Livewire\Admin\AdminDashboardComponent::class)->name('admin.dashboard');
+});
+
+require __DIR__.'/auth.php';
